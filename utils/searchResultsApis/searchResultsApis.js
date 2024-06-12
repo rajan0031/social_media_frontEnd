3ba1d92d85e262b7b1ec836edef921a5f5c17207
rajan0031 <109027109+rajan0031@users.noreply.github.com>
2024-06-12 22:29:02 +0530

    Delete utils directory

--- utils/searchResultsApis/searchResultsApis.js
+++
@@ -1,8 +0,0 @@
-// here we are defining the host of the main api
-export const host = 'https://rksocial.onrender.com';
-
-// api routes for the searching the posts and blogs
-
-
-export const searchResults = `${host}/searchresults`;
-
